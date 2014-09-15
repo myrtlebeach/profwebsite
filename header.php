db2a8188c1988d17975b082ea01f75b702499b5a
myrtlebeach <myrtlexbeach@gmail.com>
2014-09-15 14:35:44 +0200

    PHP Creation
    Nouveau fichier php

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Page personnelle prof d'histoire</title>
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Vollkorn:400italic,400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
+	<link href='style.css' rel='stylesheet'>
+	<div id="fb-root"></div>
+</head>
+<body>
+
+
+<div class="divider">
+
+	<div class="parent">
+	<h1><?php echo $title ?></h1>
+
+	<img id="joconde" src="joconde.jpg" alt="joconde de duchamps">
+	<div class=hidden message>
+	<h2><?php echo $description ?></h2>
+	<meta name="description" content="<?php echo $description ?>">
+	</div>
+	</div>
+
+</div>
--- index.php
+++ index.php
@@ -1,58 +1,50 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Page personnelle prof d'histoire</title>
-	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-	<link href='http://fonts.googleapis.com/css?family=Vollkorn:400italic,400,700' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900' rel='stylesheet' type='text/css'>
-	<link href='style.css' rel='stylesheet'>
 
-</head>
-<body>
+<?php $title = "je vous ai compris"; ?>
+<?php $description = "best blog ever"; ?>
+<?php include "header.php"; ?>
 
-	<h1>je vous ai compris</h1>
-	<img class="joconde" src="/Users/william/Desktop/joconde.jpg" alt="joconde de duchamps">
-	<h2>l'histoire autrement</h2>
+
 	<nav>
 		<ul>
-			<li>Reines rois et dirigeant</li>
-			<li>Batailles</li>
-			<li>L'inconnu du coin</li>
-			<li>le quizz du héros</li>
+			<li><a href="www.twitter.com" class="not-underlined">Reines rois et dirigeant</a></li>
+			<li><a href="www.twitter.com" class="not-underlined">Batailles</a></li>
+			<li><a href="www.twitter.com" class="not-underlined">L'inconnu du coin</a></li>
+			<li><a href="www.twitter.com" class="not-underlined">le quizz du héros</a></li>
 		</ul>
+	</nav>
+
 	<section>
 		<article class="gauche">
-			<img class="pix1" src="/Users/william/Desktop/jeff1.jpg" alt="jeff">
+			<img class="pix" src="jeff1.jpg" alt="jeff">
 			<h3>Titre A</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			<p><span class="gras">Lorem</span> ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</article>
 		<article class="gauche">
-			<img class="pix1" src="/Users/william/Desktop/jeff3.jpg" alt="jeff argent">
+			<img class="pix" src="jeff3.jpg" alt="jeff argent">
 			<h3>Titre B</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			<p><span class="gras">Lorem</span>  ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</article>
 		<article class="droite">
-			<img class="pix2" src="/Users/william/Desktop/jeff2.jpg" alt="jeff lapin">
+			<img class="pix" src="jeff2.jpg" alt="jeff lapin">
 			<h3>Titre C</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			<p><span class="gras">Lorem</span>  ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</article>
 		<article class="droite">
-			<img class="pix2" src="/Users/william/Desktop/jeff4.jpg" alt="jeff gaga">
+			<img class="pix" src="jeff4.jpg" alt="jeff gaga">
 			<h3>Titre D</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			<p><span class="gras">Lorem</span>  ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
@@ -79,7 +71,13 @@
 		<a class="stocktwits" href="http://stocktwits.com/StockTwits" id="stocktwits-follow-button"><img src="http://stocktwits.com/assets/badges/FOLLOW_US_SILVER.png" alt="Follow Us on StockTwits"/></a>
 	</aside>
 </body>
-
+</figure>
+				<figure class="effect-3">
+					<figcaption>
+						<h2><span><a id="xxx" href="www.twitter.com">xxx</a></span></h2>
+						<p>Good bye</p>
+					</figcaption>			
+</figure>
 <footer> 
 	<h4>Mentions légales</h4>
 	<p>Mes tweets n'engagent que moi!</p>
